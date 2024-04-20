9770728d9954606b1ff0627af5f7110858264c42
KCA2828 <102121878+KCA2828@users.noreply.github.com>
2024-04-19 23:26:01 -0600

    Create com.proyectoFinal.dao.RolDao.java

---
+++ com.proyectoFinal.dao.RolDao.java
@@ -0,0 +1,8 @@
+package com.proyectoFinal.dao;
+
+import com.proyectoFinal.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
